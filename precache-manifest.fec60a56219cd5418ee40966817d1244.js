9f5f1b5a55a85bdbd4e770d9bf5b3f948023c563
Axel von Engel <a.vonengel@gmail.com>
2019-08-25 13:59:53 +0200

    Deploy alpha state on 2019-08-25

---
+++ precache-manifest.fec60a56219cd5418ee40966817d1244.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3eda2d8320791de8f9bbab2790a86e",
+    "url": "/anno1800/index.html"
+  },
+  {
+    "revision": "44739b841273d869079b",
+    "url": "/anno1800/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "1c8c590bc2e43815fcca",
+    "url": "/anno1800/static/js/2.4392e13e.chunk.js"
+  },
+  {
+    "revision": "44739b841273d869079b",
+    "url": "/anno1800/static/js/main.cd93e063.chunk.js"
+  },
+  {
+    "revision": "d85fc8dc8d8c67fbe391",
+    "url": "/anno1800/static/js/runtime~main.8a0847d6.js"
+  }
+]);
