7825655b94afcd8ee3f809645cf585bd58406709
Axel von Engel <a.vonengel@gmail.com>
2019-09-01 22:14:38 +0200

    Add public service type interface

---
+++ src/data/publicServiceTypes.ts
@@ -0,0 +1,8 @@
+export interface PublicServiceAsset {
+    name?: string;
+    guid: number;
+    baseGuid?: number;
+    associatedRegions: string;
+    output?: number;
+    cycleTime?: number;
+}
