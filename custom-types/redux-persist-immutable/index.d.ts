0bc953fc4f5144a32e5905c615ca2164b42997b3
Axel von Engel <a.vonengel@gmail.com>
2019-09-02 18:57:39 +0200

    Delete unused stuff

--- custom-types/redux-persist-immutable/index.d.ts
+++
@@ -1,8 +0,0 @@
-import {Store, StoreEnhancer} from "redux";
-import {AutoRehydrateConfig, OnComplete, Persistor, PersistorConfig} from "redux-persist";
-
-declare module "redux-persist-immutable" {
-    export function autoRehydrate<State>(autoRehydrateConfig?: AutoRehydrateConfig): StoreEnhancer<State>;
-
-    export function persistStore<State>(store: Store<State>, persistorConfig?: PersistorConfig, onComplete?: OnComplete<Partial<State>>): Persistor;
-}
