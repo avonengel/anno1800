14e03aaafde9ad199fd2a6f24351a34305986202
Axel von Engel <a.vonengel@gmail.com>
2019-09-11 22:32:29 +0200

    Delete ProductCard.test.tsx

--- src/components/ProductCard.test.tsx
+++
@@ -1,6 +0,0 @@
-import {ALL_FACTORIES, Factory, ProductAsset} from "../data/assets";
-import {initialState} from "../redux/store";
-import {PRODUCTS} from "../data/productAssets";
-import {updateFactoryCount} from "../redux/production/actions";
-import {factoryProductionConsumptionReducer, factoryReducer} from "../redux/production/reducers";
-
