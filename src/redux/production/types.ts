e4574063942a9b29ac4b271dbd32107ee2365556
Axel von Engel <a.vonengel@gmail.com>
2019-09-17 20:07:18 +0200

    Remove dead code

--- src/redux/production/types.ts
+++ src/redux/production/types.ts
@@ -1,31 +1,3 @@
-export const UPDATE_FACTORY_COUNT = 'UPDATE_FACTORY_COUNT';
-
-interface UpdateFactoryCountAction {
-    type: typeof UPDATE_FACTORY_COUNT,
-    payload: UpdateFactoryCountPayload
-}
-
-export interface UpdateFactoryCountPayload {
-    islandId: number,
-    factoryId: number,
-    count: number,
-}
-
-export const UPDATE_FACTORY_PRODUCTIVITY = 'UPDATE_FACTORY_PRODUCTIVITY';
-
-interface UpdateFactoryProductivityAction {
-    type: typeof UPDATE_FACTORY_PRODUCTIVITY,
-    payload: UpdateFactoryProductivityPayload
-}
-
-export interface UpdateFactoryProductivityPayload {
-    islandId: number,
-    factoryId: number,
-    productivity: number,
-}
-
-export type ProductionActionTypes = UpdateFactoryCountAction | UpdateFactoryProductivityAction
-
 export type Consumption = number;
 
 export interface FactoryState {
