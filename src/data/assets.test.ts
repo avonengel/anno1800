c29d2a627a76b59387e24617950ff7842daa8bbc
Axel von Engel <a.vonengel@gmail.com>
2019-09-30 12:18:48 +0200

    Fix test numbers

--- src/data/assets.test.ts
+++ src/data/assets.test.ts
@@ -6,49 +6,49 @@ describe('getPopulation', () => {
         const asset = getPopulationLevelByName("Farmers");
         const enabledProducts = asset.inputs.map(input => input.product);
         // Act
-        const population = getPopulation(asset, 1, enabledProducts);
+        const population = getPopulation(asset, 100, enabledProducts);
 
         // Assert
-        expect(population).toBe(10);
+        expect(population).toBe(1000);
     });  
     it('should compute 20 for full worker house', () => {
         // Arrange
         const asset = getPopulationLevelByName("Workers");
         const enabledProducts = asset.inputs.map(input => input.product);
         // Act
-        const population = getPopulation(asset, 1, enabledProducts);
+        const population = getPopulation(asset, 100, enabledProducts);
 
         // Assert
-        expect(population).toBe(20);
+        expect(population).toBe(2000);
     });
     it('should compute 30 for full artisan house', () => {
         // Arrange
         const asset = getPopulationLevelByName("Artisans");
         const enabledProducts = asset.inputs.map(input => input.product);
         // Act
-        const population = getPopulation(asset, 1, enabledProducts);
+        const population = getPopulation(asset, 100, enabledProducts);
 
         // Assert
-        expect(population).toBe(30);
+        expect(population).toBe(3000);
     });
     it('should compute 40 for full engineer house', () => {
         // Arrange
         const asset = getPopulationLevelByName("Engineers");
         const enabledProducts = asset.inputs.map(input => input.product);
         // Act
-        const population = getPopulation(asset, 1, enabledProducts);
+        const population = getPopulation(asset, 100, enabledProducts);
 
         // Assert
-        expect(population).toBe(40);
+        expect(population).toBe(4000);
     });
     it('should compute 50 for full investor house', () => {
         // Arrange
         const asset = getPopulationLevelByName("Investors");
         const enabledProducts = asset.inputs.map(input => input.product);
         // Act
-        const population = getPopulation(asset, 1, enabledProducts);
+        const population = getPopulation(asset, 1000, enabledProducts);
 
         // Assert
-        expect(population).toBe(50);
+        expect(population).toBe(50000);
     });
 });
